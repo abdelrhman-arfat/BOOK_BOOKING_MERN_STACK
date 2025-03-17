733327f3330d21a339f6b0901843f371fa75dc46
boody <abdoarfat2006@gmail.com>
2025-03-17 21:42:33 +0200

    logout solve issue

--- backend/middleware/deleteTokens.js
+++ backend/middleware/deleteTokens.js
@@ -1,5 +1,6 @@
 export const deleteTokens = async (req, res, next) => {
-  res.clearCookie("token");
-  res.clearCookie("refreshToken");
+  res.clearCookie("token", { sameSite: "None", secure: true });
+  res.clearCookie("refreshToken", { sameSite: "None", secure: true });
+
   next();
 };
